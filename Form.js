1490b40e71c27d16bbff02c9ac04ac68f1573e97
Elísio Mualumene <elisiogabrielmualumene@gmail.com>
2022-01-11 20:53:58 +0100

    form

---
+++ Form.js
@@ -0,0 +1,19 @@
+let input = document.querySelector('.password')
+let placeholder = document.querySelector('.password').placeholder
+let button = document.querySelector('.submit')
+let div = document.querySelector('.div')
+
+var message = []
+
+button.addEventListener('click', () =>{
+     if(input.value == 'elisio'){
+        window.location.href = './main/index.html'
+    }
+
+    if (input.value != 'elisio' || input.value == '' || input.value == null){
+        message.push('SENHA INCORRETA!')
+        div.innerHTML = message
+        message = []
+    }
+    
+})
