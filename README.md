ae4a583fd85275cc68b80316d6040bd60bf35112
Elísio Mualumene <elisiogabrielmualumene@gmail.com>
2022-01-11 19:09:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootstrap Site
+ A Responsive Web Site
---
+++ index.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    window.scroll(00,700)
+})
+
+
+
+
