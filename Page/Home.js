0e465d5ce6e74c944d31d3451ea66ebe1651189f
elisiomualumene <elisiomualumene@gmail.com>
2022-01-12 23:06:10 +0100

    now for real I finish

--- Page/Home.js
+++ Page/Home.js
@@ -5,5 +5,17 @@ button.addEventListener('click', () => {
 })
 
 
+let spinner2 = document.querySelector('.spinner2')
+let show2 = document.querySelector('#show2')
 
 
+var cache2
+
+function spin2() {
+    cache2 = setTimeout(showpage2, 1200)
+}
+
+function showpage2(){
+    spinner2.style.display = 'none'
+    show2.style.display = 'block'
+}
